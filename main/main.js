44ec3a553b62fe029c5c94f8058c9ca98cfec0bd
SimonZoo <simonzoo@yeah.net>
2019-05-30 15:30:03 +0800

    Add calculate taxi fair

--- main/main.js
+++ main/main.js
@@ -1,4 +1,11 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    let distance = inputs["distance"], parkTime = inputs["parkTime"], price = 6
+    if(distance > 2 && distance <= 8) {
+        price += 0.8 * (distance - 2)
+    } else if (distance > 8) {
+        // 4.8 = (8 - 2) * 0.8
+        price += 4.8 + 1.2*(distance - 8)
+    }
+    price += 0.25 * parkTime
+    return Math.round(price)
 };
